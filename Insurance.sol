c4d653c1790196b4aef41c320dfa0129d9bf4c4a
NajdBinathel <78319814+NajdBinathel@users.noreply.github.com>
2021-03-06 23:42:13 +0300

    Update Insurance.sol

--- Insurance.sol
+++ Insurance.sol
@@ -30,33 +30,46 @@ contract TechInsurance {
     uint productCounter;
     
     address payable insOwner;
-    constructor(address payable _insOwner) public{
-        insOwner = _insOwner;
+    constructor(address payable _insOwner) public ERC721("Elite", "code"){
+      insOwner = _insOwner;
     }
  
     function addProduct(uint _productId, string memory _productName, uint _price ) public {
         Product memory newproduct = Product (_productId , _productName , _price ,false, msg.sender);
         insOwner[msg.sender] = newproduct;
-        productId[productCounter] = newproduct;
+        productId[productCounter++] = newproduct;
     }
     
     
     function changeFalse(uint _productIndex) public {
-               require (msg.value == productIndex[_productIndex]).changeFalse[_offered];   
+               require(msg.sender == insOwner, "sorry");
+        return productIndex[_productIndex].offered == false;   
     }
     function changeTrue(uint _productIndex) public {
-        require (msg.value == productIndex[_productIndex]).changeTrue[_offered];   
+           require(msg.sender == insOwner, "Approve ");
+        return productIndex[_productIndex].offered ==true;  
     }
     
     function changePrice(uint _productIndex, uint _price) public {
-       require (msg.value == productIndex[_productIndex]).changePrice[_price];
+      require(productIndex[_productIndex].price >= 1, "eror not valid" );
+        productIndex[_productIndex].price== _price;
 
     }
     
     function clientSelect(uint _productIndex) public payable {
-        if( isValid == true ){
-            return _productIndex;
+        require(productIndex[_productIndex].price == msg.value, "eror" );
+        require( productIndex[_productIndex].price == 0, "eroor number index");
+        
+        Client memory newClient;
+        newClient.isValid = true;
+        newClient.time = block.number;
+        client[msg.sender][_productIndex] = newClient;
+        insOwner.transfer(msg.value);
         }
     } 
+     function setPriceByOwner (uint _price) public {
+        uint price = _price;
+        require(insOwner == msg.sender, "eror, just the owner can set");
+    }
     
 }
